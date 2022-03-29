3a7e849963c6623871bb96993a292e2240c2b28e
Tina-Bambina <speleerska@gmail.com>
2022-03-29 18:47:54 +0200

    [FEATURE] car owner data structure

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,26 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
+let auto = {
+    znacka: 'Ford',
+    barva: 'modra',
+    rokVyroby: 2015,
+    spz: '1B2 3456'
 
+
+
+};
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
 
+auto.majitel = {};
+auto.majitel.jmeno = 'Jana';
+auto.majitel.prijmeni = 'Czechita';
+
+
 
 // Přepiš značku na Škoda
-
+auto['znacka'] = 'SKODA';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+let majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijmeni;
+let znacka = auto.znacka;
+let spz = auto.spz;
